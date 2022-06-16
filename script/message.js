92ac776ca052ecd1fe1b1df96ed5cec988909073
William Robert-Coyle <willrobertcoyle22@gmail.com>
2022-06-16 11:46:40 -0500

    Ajout fonction ouvrir et fermer boite modale

--- script/message.js
+++ script/message.js
@@ -1,9 +1,22 @@
 (function(){
 
-    let elboite__modale = document.createElement('div');
-    elboite__modale.classList.add('boite__modale');
-    let elBody = document.querySelector('body');
-    elBody.appendChild(elboite__modale);
+    let elBoite__modale;
+    let elBoite__x;
+
+    function creerBoiteModale() {
+        elBoite__modale = document.createElement('div');
+        elBoite__x = document.createElement('button');
+
+        elBoite__modale.appendChild(elBoite__x);
+        elBoite__x.classList.add('boite__x');
+        elBoite__x.innerHTML = 'X ';
+
+        elBoite__modale.classList.add('boite__modale');
+        let elBody = document.querySelector('body');
+        elBody.appendChild(elBoite__modale);
+    }
+    
+    creerBoiteModale();
 
     // document.querySelector('.boite__modale');
 
@@ -11,6 +24,13 @@
     elBtn.addEventListener('mousedown', function() {
 
         console.log('Bravo le script fonctionne ... KEKW');
+        elBoite__modale.classList.add('boite__modale__ouvrir');
+
+    });
+
+    elBoite__x.addEventListener('mousedown', function() {
+
+        elBoite__modale.classList.remove('boite__modale__ouvrir');
 
     });
 
