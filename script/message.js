384e939d9361e0e88cc9b2014c063dcd1b0063ff
William Robert-Coyle <willrobertcoyle22@gmail.com>
2022-06-16 10:48:39 -0500

    s5-c10 debut de l'affichage boite modale

--- script/message.js
+++ script/message.js
@@ -1,5 +1,12 @@
 (function(){
-                    
+
+    let elboite__modale = document.createElement('div');
+    elboite__modale.classList.add('boite__modale');
+    let elBody = document.querySelector('body');
+    elBody.appendChild(elboite__modale);
+
+    // document.querySelector('.boite__modale');
+
     let elBtn = document.querySelector('#mon_bouton');
     elBtn.addEventListener('mousedown', function() {
 
