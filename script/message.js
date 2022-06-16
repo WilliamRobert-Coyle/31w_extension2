84e31103d3b7c6652e09ced82a1355be56e89e1e
William Robert-Coyle <willrobertcoyle22@gmail.com>
2022-06-16 10:27:18 -0500

    Debut de l'extension de l'exercice 2

---
+++ script/message.js
@@ -0,0 +1,10 @@
+(function(){
+                    
+    let elBtn = document.querySelector('#mon_bouton');
+    elBtn.addEventListener('mousedown', function() {
+
+        console.log('Bravo le script fonctionne ... KEKW');
+
+    });
+
+})()
